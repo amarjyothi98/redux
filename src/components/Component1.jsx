9920680eda4679b0aa866b0b4cd009320592d0c9
amarjyothi98 <amarjyothi526@gmail.com>
2023-12-05 06:12:19 +0530

    added store globally

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Component1 = () => {
+
+    const [name, setName] = useState('');
+    const [anything, setAnything] = useState('');
+
   return (
     <div>
-      <input type="text" placeholder='name' />
-      <input type="text" placeholder='anthing else' />
+      <input type="text" placeholder='name' onChange={e => setName(e.target.value)}/>
+      <input type="text" placeholder='anthing else' onChange={e => setAnything(e.target.value)}/>
+
+      <p>{name}</p>
+      <p>{anything}</p>
     </div>
   )
 }
--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -5,5 +5,15 @@ export const userSlice = createSlice({
     initialState: {
         name: "amar",
         email: "initial@gmail.com",
+    }, 
+    reducers: {
+        update: (state, action) => {
+            //action.payload = {name, email} 
+            state.name = action.payload.name;
+            state.email = action.payload.email;
+        }
     }
 })
+
+export const { update } = userSlice.actions;
+export default userSlice.reducer;
